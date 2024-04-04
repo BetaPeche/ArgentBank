c16c336a11b74cbc3f3c8516c23075867ee8dc2a
BetaPeche <lolopellegrino@gmail.com>
2024-04-04 17:48:30 +0200

    Add username link

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,18 @@
-import { Link } from "react-router-dom"
+import { useDispatch, useSelector } from "react-redux"
+import { Link, useNavigate } from "react-router-dom"
+import { logOut } from "../features/userSlice"
 
 const Header = () => {
+    const user = useSelector((state) => state.user)
+    const dispatch = useDispatch()
+    const navigate = useNavigate()
+
+    const handleSignOut = (event) => {
+        event.preventDefault()
+        dispatch(logOut())
+        navigate("/")
+    }
+
     return (
         <nav className="main-nav">
             <Link to="/" className="main-nav-logo">
@@ -12,10 +24,27 @@ const Header = () => {
                 <h1 className="sr-only">Argent Bank</h1>
             </Link>
             <div>
-                <Link to="/sign-in" className="main-nav-item">
-                    <i className="fa fa-user-circle"></i>
-                    &nbsp;Sign In
-                </Link>
+                {user.isConnected ? (
+                    <>
+                        <Link to="/profile" className="main-nav-item">
+                            <i className="fa fa-user-circle"></i>
+                            &nbsp;{user.userName}&nbsp;
+                        </Link>
+                        <a
+                            href="/"
+                            className="main-nav-item"
+                            onClick={handleSignOut}
+                        >
+                            <i className="fa fa-sign-out"></i>
+                            &nbsp;Sign Out
+                        </a>
+                    </>
+                ) : (
+                    <Link to="/login" className="main-nav-item">
+                        <i className="fa fa-user-circle"></i>
+                        &nbsp;Sign In
+                    </Link>
+                )}
             </div>
         </nav>
     )
