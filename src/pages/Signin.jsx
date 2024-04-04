48053533bbe736a20cbf6a262b289d55104eb69f
BetaPeche <lolopellegrino@gmail.com>
2024-04-04 17:49:23 +0200

    Use redux toolkit

--- src/pages/Signin.jsx
+++ src/pages/Signin.jsx
@@ -1,12 +1,16 @@
+import { useState } from "react"
 import { useNavigate } from "react-router-dom"
+import { fetchUserProfile, logIn } from "../features/userSlice"
+import { useDispatch } from "react-redux"
 
 const Signin = () => {
+    const dispatch = useDispatch()
     const navigate = useNavigate()
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
 
     const handleSubmit = async (event) => {
         event.preventDefault()
-        const email = document.getElementById("username").value
-        const password = document.getElementById("password").value
 
         try {
             const response = await fetch(
@@ -22,11 +26,23 @@ const Signin = () => {
                     }),
                 }
             )
-
             const res = await response.json()
+
             if (res.status === 200) {
-                console.log(res.body.token)
-                navigate("/user")
+                dispatch(logIn(res.body.token))
+                const response2 = await fetch(
+                    "http://localhost:3001/api/v1/user/profile",
+                    {
+                        method: "POST",
+                        headers: {
+                            "Content-Type": "application/json",
+                            Authorization: `Bearer ${res.body.token}`,
+                        },
+                    }
+                )
+                const res2 = await response2.json()
+                dispatch(fetchUserProfile(res2.body))
+                navigate("/profile")
             }
             if (res.status === 400) {
                 console.log("Erreur")
@@ -41,23 +57,30 @@ const Signin = () => {
             <section className="sign-in-content">
                 <i className="fa fa-user-circle sign-in-icon"></i>
                 <h1>Sign In</h1>
-                <form>
+                <form onSubmit={handleSubmit}>
                     <div className="input-wrapper">
                         <label htmlFor="username">Username</label>
-                        <input type="text" id="username" />
+                        <input
+                            type="text"
+                            id="username"
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)}
+                        />
                     </div>
                     <div className="input-wrapper">
                         <label htmlFor="password">Password</label>
-                        <input type="password" id="password" />
+                        <input
+                            type="password"
+                            id="password"
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+                        />
                     </div>
                     <div className="input-remember">
                         <input type="checkbox" id="remember-me" />
                         <label htmlFor="remember-me">Remember me</label>
                     </div>
-                    {/* <a href="./user.html" className="sign-in-button">
-                        Sign In
-                    </a> */}
-                    <button className="sign-in-button" onClick={handleSubmit}>
+                    <button className="sign-in-button" type="submit">
                         Sign In
                     </button>
                 </form>
