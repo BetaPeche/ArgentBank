693a09ca9d44a08e253b15db0542671fc2a9e905
BetaPeche <lolopellegrino@gmail.com>
2024-04-04 17:48:55 +0200

    Add user reducers

--- src/features/userSlice.js
+++ src/features/userSlice.js
@@ -3,9 +3,38 @@ import { createSlice } from "@reduxjs/toolkit";
 export const userSlice = createSlice({
     name: "user",
     initialState: {
-        user: null,
+        id: null,
+        firstName: null,
+        lastName: null,
+        userName: null,
+        email: null,
+        token: null,
+        isConnected: false,
     },
     reducers: {
-
+        logIn: (state, action) => {
+            state.token = action.payload
+            state.isConnected = true
+        },
+        fetchUserProfile: (state, action) => {
+            state.id = action.payload.id
+            state.firstName = action.payload.firstName
+            state.lastName = action.payload.lastName
+            state.userName = action.payload.userName
+            state.email = action.payload.email
+        },
+        logOut: (state) => {
+            state.id = null
+            state.firstName = null
+            state.lastName = null
+            state.userName = null
+            state.email = null
+            state.token = null
+            state.isConnected = false
+        }
     }
 })
+
+export const { logIn, fetchUserProfile, logOut } = userSlice.actions
+
+export default userSlice.reducer
